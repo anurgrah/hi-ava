11047ce522f36681d1a2c85209799bcb3b23b83e
anurgrah <64807569+anurgrah@users.noreply.github.com>
2020-05-05 07:25:59 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -2,7 +2,7 @@
 export default [
     { author: "Dari Pacarmu Anugrah", quotes: "Halo Sayang " },
     { author: "Dari Pacarmu Anugrah", quotes: "Gimane Kabar Eee? " },
-    { author: "Dari Pacarmu Anugrah", quotes: "Bebiakan Yuk Kalok Nak Chat La" },
+    { author: "Dari Pacarmu Anugrah", quotes: "Dapat Salam Dari Yang Punya Web :)" },
     { author: "Dari Pacarmu Anugrah", quotes: "I love you." },
     { author: "Dari Pacarmu Frozers", quotes: "Mau Gak Jadi Pacar AKU?" },
     { author: "Anugrah Pratama", quotes: "I Love You" },
